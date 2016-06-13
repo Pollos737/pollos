a81407ecfe39a2b24bf26456bff792221a07fb5e
Wilmer <Wilmer>
2016-06-12 21:08:31 -0700

    added basic tags for employee profile view

--- views/site/employeeprofile.php
+++ views/site/employeeprofile.php
@@ -12,4 +12,18 @@ $this->title = 'Pollos User Profile';
 <div>
     <h1><?=Html::encode($this->title)?></h1>
     <P>This is where your picture will go</P>
+    <div class="jumbotron">
+        <div class="image">
+            This is where the Image Will Go
+        </div>
+        <div class="bio">
+            This is where the Bio WIll Go
+        </div>
+        <div class="friends">
+            This is where friends will go
+        </div>
+        <div class="comments">
+            This is where comments will go
+        </div>
+    </div>
 </div>
